99088f9363553bb041eb5cb063653f03f74f1863
greenkoopa <green.koopa667@gmail.com>
2017-11-29 17:09:28 -0500

    Getting text outputs

--- getAnime.py
+++ getAnime.py
@@ -1,6 +1,12 @@
 import requests
+from xml.etree import ElementTree
 
 url = "http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=4658"
 response = requests.get(url)
 
-print(response.content);
+tree = ElementTree.fromstring(response.content)
+
+for child in tree[0]:
+    text = child.itertext()
+    for t in text:
+        print(t)
