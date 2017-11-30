0edf9a2d84f2b3a958fd777a3392a1af1c38ee24
greenkoopa <green.koopa667@gmail.com>
2017-11-30 17:13:09 -0500

    Progress parsing. Cant get inner attribute elements

--- getAnime.py
+++ getAnime.py
@@ -5,8 +5,22 @@ url = "http://cdn.animenewsnetwork.com/encyclopedia/api.xml?anime=4658"
 response = requests.get(url)
 
 tree = ElementTree.fromstring(response.content)
+#print(tree)
 
-for child in tree[0]:
-    text = child.itertext()
-    for t in text:
-        print(t)
+for child in tree:
+    #x = child.findall('info')
+    #print (x)
+    #if x != None:
+    #    print(child.attrib)
+    for anime in child:
+        text = anime.itertext()
+        if anime.tag == 'info':
+            #print("This is info")
+            att = anime.attrib
+            print(att['type'])
+        #print(x)
+        #print(att.tag)
+        #if x != None:
+            #print(att.attrib)
+        #for t in text:
+        #    print(t)
